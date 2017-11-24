abcccdf55a676a362de955725df00c6f8246781f
Nicholas Kajoh <kajohterna@gmail.com>
2017-11-24 11:16:32 +0100

    add C++ Stack

--- stacks/stack.cpp
+++ stacks/stack.cpp
@@ -2,9 +2,65 @@
 
 using namespace std;
 
+#define MAX 1000
+
+class stack {
+  private:
+    int top;
+    int a[MAX];
+
+  public:
+    stack();
+    void push(int);
+    int pop();
+    bool is_empty();
+    int peek();
+};
+
+stack::stack() {
+  top = -1;
+}
+
+void stack::push(int x) {
+  a[++top] = x;
+}
+
+int stack::pop() {
+  if(this->is_empty()) {
+    cout << "Stack underflow!" << endl;
+    return 0;
+  } else {
+    int x = a[top--];
+    return x;
+  }
+}
+
+bool stack::is_empty() {
+  return top < 0;
+}
+
+int stack::peek() {
+  if(this->is_empty()) {
+    cout << "Stack is empty!" << endl;
+    return 0;
+  } else {
+    return a[top];
+  }
+}
+
+
+// test
 int main() {
-  cout << "Hello Stack!";
-  
-  std::cin.get();
+  stack my_stack;
+  cout << my_stack.is_empty() << endl;
+  my_stack.push(12);
+  my_stack.push(-1);
+  cout << my_stack.peek() << endl;
+  my_stack.push(0);
+  cout << my_stack.pop() <<  endl;
+  cout << my_stack.pop() <<  endl;
+  cout << my_stack.peek() << endl;
+  cout << my_stack.is_empty() << endl;
+
   return 0;
 }
