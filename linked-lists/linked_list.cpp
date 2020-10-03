0bc7fbbf0a27bbe88d9042ccad23833ac5012f6b
koustav01 <71479361+koustav01@users.noreply.github.com>
2020-10-03 21:27:58 +0530

    Update linked_list.cpp

--- linked-lists/linked_list.cpp
+++ linked-lists/linked_list.cpp
@@ -19,11 +19,15 @@ int main() {
   linked_list = new node(-1, linked_list);
   linked_list = new node(7, linked_list);
 
+ vector<int>v;
   // print out linked list
   while(linked_list != NULL) {
     cout << linked_list->data << endl;
+    v.push_back(linked_list->data); // storing elements in an vector for printing in reverse manner since we cannot traverse in reverse direction in singly linked list. 
     linked_list = linked_list->next;
   }
-
+  //Printing output in reverse order
+  for (auto ir = v.rbegin(); ir != v.rend(); ++ir) 
+        cout << *ir << " "; 
   return 0;
 }
